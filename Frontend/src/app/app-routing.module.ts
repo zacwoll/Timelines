743617b65f7631538e927ad03c4cee998879d5fe
Lora Reames <shes.lora.reames@gmail.com>
2023-10-04 18:34:03 -0500

    Created Landing Page, Added Routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/landing-page",
+    pathMatch: "full",
+    // canActivate: [AuthGuard], TODO: Implement AuthGuard
+  },
+  {
+    path: "landing-page",
+    component: LandingPageComponent,
+  },
+  // Define routes for individual user pages (e.g., /userID)
+  // {
+  //   path: ":userID",
+  //   component: UserProfileComponent, // Replace with your user profile component
+  // },
+  // Other routes as needed
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
